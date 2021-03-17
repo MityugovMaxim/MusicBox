078f2dd21a1e6b4e9f57e144458a4b5f0aec78a0
MityugovMaxim <mityugovmaxim@gmail.com>
2021-03-17 16:33:04 +0300

    Removed AudioUtility.cs

--- Assets/Scripts/Sequencer/Clips/MusicClip.cs
+++ Assets/Scripts/Sequencer/Clips/MusicClip.cs
@@ -19,49 +19,21 @@ public class MusicClip : Clip
 
 	protected override void OnEnter(float _Time)
 	{
+		if (!Sequencer.Playing || !Playing)
+			return;
+		
 		float time = Mathf.Clamp(GetLocalTime(_Time) + m_MinOffset, 0, m_AudioClip.length);
 		
-		#if UNITY_EDITOR
-		if (!Application.isPlaying && Sequencer.Playing && Playing)
-		{
-			AudioUtility.PlayClip(m_AudioClip);
-			AudioUtility.SetClipSamplePosition(m_AudioClip, time);
-			return;
-		}
-		#endif
+		m_AudioSource.clip = m_AudioClip;
 		
-		if (Sequencer.Playing && Playing)
-		{
-			m_AudioSource.clip = m_AudioClip;
-			
-			m_AudioSource.Play();
-			
-			m_AudioSource.time = time;
-		}
+		m_AudioSource.Play();
+		
+		m_AudioSource.time = time;
 	}
 
 	protected override void OnUpdate(float _Time)
 	{
 		float time = Mathf.Clamp(GetLocalTime(_Time) + m_MinOffset, 0, m_AudioClip.length);
-		
-		#if UNITY_EDITOR
-		if (!Application.isPlaying)
-		{
-			if (!Sequencer.Playing && Playing)
-			{
-				AudioUtility.StopClip(m_AudioClip);
-				AudioUtility.SetClipSamplePosition(m_AudioClip, time);
-			}
-			else if (Sequencer.Playing && Playing && !AudioUtility.IsClipPlaying(m_AudioClip))
-			{
-				AudioUtility.PlayClip(m_AudioClip);
-				AudioUtility.SetClipSamplePosition(m_AudioClip, time);
-			}
-			return;
-		}
-		#endif
-		
-		m_AudioSource.clip = m_AudioClip;
 		
 		if (!Sequencer.Playing && Playing)
 		{
@@ -71,6 +43,8 @@ public class MusicClip : Clip
 		}
 		else if (Sequencer.Playing && Playing && !m_AudioSource.isPlaying)
 		{
+			m_AudioSource.clip = m_AudioClip;
+			
 			m_AudioSource.Play();
 			
 			m_AudioSource.time = time;
@@ -79,14 +53,6 @@ public class MusicClip : Clip
 
 	protected override void OnExit(float _Time)
 	{
-		#if UNITY_EDITOR
-		if (!Application.isPlaying)
-		{
-			AudioUtility.StopClip(m_AudioClip);
-			return;
-		}
-		#endif
-		
 		m_AudioSource.Stop();
 	}
 }
