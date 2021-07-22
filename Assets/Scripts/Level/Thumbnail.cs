2b1818c91eef67f7bef1295d3f19049557030598
MityugovMaxim <mityugovmaxim@gmail.com>
2021-07-23 02:16:12 +0300

    Fixes

---
+++ Assets/Scripts/Level/Thumbnail.cs
@@ -0,0 +1,30 @@
+using System;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Zenject;
+
+public class Thumbnail : UIEntity, IPointerClickHandler
+{
+	public RectTransform Mount => m_Mount;
+
+	public event Action OnClick;
+
+	RectTransform m_Mount;
+
+	[Inject]
+	public void Construct(RectTransform _Mount)
+	{
+		m_Mount = _Mount;
+		
+		RectTransform.SetParent(_Mount, false);
+	}
+
+	public void OnPointerClick(PointerEventData _EventData)
+	{
+		OnClick?.Invoke();
+	}
+
+	[UsedImplicitly]
+	public class Factory : PlaceholderFactory<string, RectTransform, Thumbnail> { }
+}
