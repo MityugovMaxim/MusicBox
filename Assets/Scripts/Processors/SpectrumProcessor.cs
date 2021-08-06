b64639f8765a50b35a3bb743d9637beac7af486b
MityugovMaxim <mityugovmaxim@gmail.com>
2021-08-06 14:49:35 +0300

    Fixed EQ fx

--- Assets/Scripts/Processors/SpectrumProcessor.cs
+++ Assets/Scripts/Processors/SpectrumProcessor.cs
@@ -1,54 +1,47 @@
 using UnityEngine;
+using Zenject;
 
 [RequireComponent(typeof(AudioSource))]
 public class SpectrumProcessor : MonoBehaviour
 {
 	static readonly int m_SpectrumPropertyID = Shader.PropertyToID("_Spectrum");
 
-	[SerializeField] float m_SpectrumDampenStep;
-	[SerializeField] float m_AmplitudeDampenStep;
+	[SerializeField] float m_DampenStep = 0.005f;
+	[SerializeField] float[] m_Amplitude;
 
-	AudioSource m_AudioSource;
+	SignalBus      m_SignalBus;
+	LevelProcessor m_LevelProcessor;
+	AudioSource    m_AudioSource;
 
 	readonly float[] m_Buffer          = new float[64];
-	readonly float[] m_Amplitude       = new float[64];
 	readonly float[] m_Spectrum        = new float[64];
-	readonly float[] m_SpectrumDampen  = new float[64];
-	readonly float[] m_AmplitudeDampen = new float[64];
+	readonly float[] m_SpectrumDampen  = new float[32];
 
 	void Awake()
 	{
 		m_AudioSource = GetComponent<AudioSource>();
 		
-		for (int i = 0; i < m_Amplitude.Length; i++)
-			m_Amplitude[i] = float.Epsilon * 2;
+		if (m_Amplitude == null || m_Amplitude.Length != 32)
+			m_Amplitude = new float[32];
 	}
 
 	void Update()
 	{
 		m_AudioSource.GetSpectrumData(m_Buffer, 0, FFTWindow.BlackmanHarris);
 		
-		for (int i = 0; i < m_Spectrum.Length; i++)
+		for (int i = 0; i < 32; i++)
 			m_Spectrum[i] = Mathf.Max(0, m_Spectrum[i] - m_SpectrumDampen[i]);
 		
-		// for (int i = 0; i < m_Amplitude.Length; i++)
-		// 	m_Amplitude[i] = Mathf.Max(0.001f, m_Amplitude[i] - m_AmplitudeDampen[i]);
-		
 		int index = 0;
-		for (var i = 0; i < m_Buffer.Length; i++)
+		for (var i = 0; i < 32; i++)
 		{
-			int size = 1;
-			
 			float spectrum = 0;
-			for (int j = 0; j < size; j++)
-				spectrum += m_Buffer[index++];
-			spectrum /= size;
+			spectrum += m_Buffer[index++];
+			spectrum += m_Buffer[index++];
+			spectrum *= 0.5f;
 			
 			if (m_Amplitude[i] < spectrum)
-			{
-				m_Amplitude[i]       = spectrum;
-				m_AmplitudeDampen[i] = 0;
-			}
+				m_Amplitude[i] = spectrum;
 			
 			if (spectrum > float.Epsilon && m_Amplitude[i] > float.Epsilon)
 				spectrum /= m_Amplitude[i];
@@ -59,9 +52,11 @@ public class SpectrumProcessor : MonoBehaviour
 				m_SpectrumDampen[i] = 0;
 			}
 			
-			m_SpectrumDampen[i]  += m_SpectrumDampenStep;
-			m_AmplitudeDampen[i] += m_AmplitudeDampenStep;
+			m_SpectrumDampen[i] += m_DampenStep;
 		}
+		
+		for (int i = 0; i < 32; i++)
+			m_Spectrum[32 + i] = m_Spectrum[i];
 		
 		Shader.SetGlobalFloatArray(m_SpectrumPropertyID, m_Spectrum);
 	}
