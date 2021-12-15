6f17c25ce020de9644e781178eeb70a358da70c5
MityugovMaxim <mityugovmaxim@gmail.com>
2021-12-15 19:13:42 +0300

    Notification processor cleanup

--- Assets/Scripts/Processors/NotificationProcessor.cs
+++ Assets/Scripts/Processors/NotificationProcessor.cs
@@ -8,47 +8,14 @@ public abstract class NotificationProcessor : IInitializable
 {
 	public string LaunchURL { get; private set; }
 
-	readonly OffersProcessor m_OffersProcessor;
-
 	[Inject]
-	public NotificationProcessor(OffersProcessor _OffersProcessor)
-	{
-		m_OffersProcessor = _OffersProcessor;
-	}
-
-	public void OfferNotification(string _OfferID)
-	{
-		string levelID = m_OffersProcessor.GetLevelID(_OfferID);
-		long   coins   = m_OffersProcessor.GetCoins(_OfferID);
-		string title   = m_OffersProcessor.GetTitle(_OfferID);
-		
-		bool hasLevelID = !string.IsNullOrEmpty(levelID);
-		bool hasCoins   = coins > 0;
-		
-		string text;
-		if (hasLevelID && hasCoins)
-			text = "Get your new track and free coins right now!";
-		else if (hasLevelID)
-			text = "Get your new track right now!";
-		else if (hasCoins)
-			text = "Get your free coins right now!";
-		else
-			text = "Get your reward right now!";
-		
-		ScheduleNotification(
-			"offer_" + _OfferID,
-			title,
-			text,
-			"audiobox://offers",
-			TimeSpan.FromDays(1)
-		);
-	}
+	public NotificationProcessor() { }
 
 	protected abstract string GetLaunchURL();
 
 	protected abstract void RemoveScheduledNotifications();
 
-	protected abstract void ScheduleNotification(
+	public abstract void ScheduleNotification(
 		string   _ID,
 		string   _Title,
 		string   _Subtitle,
@@ -70,7 +37,7 @@ public abstract class NotificationProcessor : IInitializable
 public class iOSNotificationProcessor : NotificationProcessor
 {
 	[Inject]
-	public iOSNotificationProcessor(OffersProcessor _OffersProcessor) : base(_OffersProcessor) { }
+	public iOSNotificationProcessor() { }
 
 	protected override string GetLaunchURL()
 	{
@@ -84,7 +51,7 @@ public class iOSNotificationProcessor : NotificationProcessor
 		iOSNotificationCenter.RemoveAllScheduledNotifications();
 	}
 
-	protected override void ScheduleNotification(
+	public override void ScheduleNotification(
 		string   _ID,
 		string   _Title,
 		string   _Subtitle,
