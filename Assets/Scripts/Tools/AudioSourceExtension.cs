af87f43e2f88da1998a3136ea04726ce6c5d3a9f
MityugovMaxim <mityugovmaxim@gmail.com>
2022-02-07 18:03:29 +0300

    Added extension for AudioSource

---
+++ Assets/Scripts/Tools/AudioSourceExtension.cs
@@ -0,0 +1,33 @@
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public static class AudioSourceExtension
+{
+	public static async Task SetVolumeAsync(this AudioSource _AudioSource, float _Volume, float _Duration, CancellationToken _Token)
+	{
+		if (_AudioSource == null || _Token.IsCancellationRequested)
+			return;
+		
+		float source = _AudioSource.volume;
+		float target = Mathf.Clamp01(_Volume);
+		
+		if (!Mathf.Approximately(source, target) && _Duration > float.Epsilon)
+		{
+			float time = 0;
+			while (time < _Duration)
+			{
+				await Task.Yield();
+				
+				if (_Token.IsCancellationRequested)
+					return;
+				
+				time += Time.deltaTime;
+				
+				_AudioSource.volume = Mathf.Lerp(source, target, time / _Duration);
+			}
+		}
+		
+		_AudioSource.volume = target;
+	}
+}
