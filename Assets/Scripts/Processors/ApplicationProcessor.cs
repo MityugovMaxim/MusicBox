9a97199cea1b8d8961acbd93007fa50716c778de
MityugovMaxim <mityugovmaxim@gmail.com>
2022-02-07 18:02:51 +0300

    Added cache cleanup into ApplicationProcessor

--- Assets/Scripts/Processors/ApplicationProcessor.cs
+++ Assets/Scripts/Processors/ApplicationProcessor.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Firebase.Database;
@@ -42,6 +43,14 @@ public class BannerSnapshot
 [Preserve]
 public class ApplicationProcessor
 {
+	const string CACHE_VERSION_KEY = "CACHE_VERSION";
+
+	static string CacheVersion
+	{
+		get => PlayerPrefs.GetString(CACHE_VERSION_KEY, string.Empty);
+		set => PlayerPrefs.SetString(CACHE_VERSION_KEY, value);
+	}
+
 	bool Loaded { get; set; }
 
 	readonly SignalBus         m_SignalBus;
@@ -70,12 +79,9 @@ public class ApplicationProcessor
 		
 		await FetchApplication();
 		
+		TryClearCache();
+		
 		Loaded = true;
-	}
-
-	public bool IsOutdated()
-	{
-		return m_ApplicationSnapshot != null && m_ApplicationSnapshot.Version != Application.version;
 	}
 
 	public List<string> GetBannerIDs()
@@ -149,6 +155,66 @@ public class ApplicationProcessor
 			foreach (DataSnapshot bannerSnapshot in bannersSnapshot.Children)
 				m_BannerSnapshots.Add(new BannerSnapshot(bannerSnapshot));
 		}
+	}
+
+	void TryClearCache()
+	{
+		(int sourceMajor, int sourceMinor, int sourcePatch) = GetVersion(CacheVersion);
+		(int targetMajor, int targetMinor, int targetPatch) = GetVersion(m_ApplicationSnapshot.Version);
+		
+		if (sourceMajor != targetMajor || sourceMinor != targetMinor || sourcePatch != targetPatch)
+			return;
+		
+		Debug.LogFormat(
+			"[ApplicationProcessor] Clear cache. Cache version: '{0}.{1}.{2}' Application version: '{3}.{4}.{5}'.",
+			sourceMajor, sourceMinor, sourcePatch,
+			targetMajor, targetMinor, targetPatch
+		);
+		
+		try
+		{
+			string[] directories = Directory.GetDirectories(Application.persistentDataPath);
+			
+			foreach (string directory in directories)
+				Directory.Delete(directory, true);
+			
+			CacheVersion = m_ApplicationSnapshot.Version;
+			
+			Debug.LogError("[ApplicationProcessor] Clear cache success.");
+		}
+		catch (Exception exception)
+		{
+			Debug.LogError("[ApplicationProcessor] Clear cache failed.");
+			Debug.LogException(exception);
+		}
+	}
+
+	(int Major, int Minor, int Build) GetVersion(string _Version)
+	{
+		if (string.IsNullOrEmpty(_Version))
+			return (0, 0, 0);
+		
+		string[] data = _Version.Split(new char[] { '.', '(', ')' }, StringSplitOptions.RemoveEmptyEntries);
+		
+		int major;
+		if (data.Length >= 1 && !string.IsNullOrEmpty(data[0]))
+			int.TryParse(data[0], out major);
+		else
+			major = 1;
+		
+		int minor;
+		if (data.Length >= 2 && !string.IsNullOrEmpty(data[1]))
+			int.TryParse(data[1], out minor);
+		else
+			minor = 0;
+		
+		int patch;
+		if (data.Length >= 3 && !string.IsNullOrEmpty(data[2]))
+			int.TryParse(data[1], out patch);
+		else
+			patch = 0;
+		
+		return (major, minor, patch);
 	}
 
 	BannerSnapshot GetBannerSnapshot(string _BannerID)
