ecc5213181116948063788a245329484740700a9
MityugovMaxim <mityugovmaxim@gmail.com>
2022-06-29 19:23:47 +0300

    Remove Unity Ads package

--- Assets/Scripts/Processors/AdsProcessor.cs
+++ Assets/Scripts/Processors/AdsProcessor.cs
@@ -7,7 +7,7 @@ using Firebase.Auth;
 using Firebase.Database;
 using MAXHelper;
 using UnityEngine;
-using UnityEngine.Advertisements;
+//using UnityEngine.Advertisements;
 using UnityEngine.Scripting;
 using Zenject;
 
@@ -89,192 +89,192 @@ public class AdsProviderAdMob : IAdsProvider
 	}
 }
 
-[Preserve]
-public class AdsProviderUnity : IAdsProvider, IUnityAdsInitializationListener, IUnityAdsLoadListener, IUnityAdsShowListener
-{
-	#if UNITY_IOS
-	static string GameID         => "4234912";
-	static string InterstitialID => "Interstitial_iOS";
-	static string RewardedID     => "Rewarded_iOS";
-	#elif UNITY_ANDROID
-	static string GameID         => "4234913";
-	static string InterstitialID => "Interstitial_Android";
-	static string RewardedID     => "Rewarded_Android";
-	#endif
-
-	string IAdsProvider.ID => "unity_ads";
-
-	readonly Dictionary<string, Action<bool>> m_LoadFinished = new Dictionary<string, Action<bool>>();
-	readonly Dictionary<string, Action<bool>> m_ShowFinished = new Dictionary<string, Action<bool>>();
-
-	Action<bool> m_LoadAdsFinished;
-	Action<bool> m_InterstitialFinished;
-	Action<bool> m_RewardedFinished;
-
-	public Task<bool> Initialize()
-	{
-		TaskCompletionSource<bool> completionSource = new TaskCompletionSource<bool>();
-		
-		m_LoadAdsFinished = _Success => completionSource.TrySetResult(_Success);
-		
-		if (Advertisement.isInitialized)
-		{
-			InvokeLoadAdsFinished(true);
-			return completionSource.Task;
-		}
-		
-		if (!Advertisement.isSupported)
-		{
-			Debug.LogError("[AdsUnity] Initialize failed. Ads not supported.");
-			InvokeLoadAdsFinished(false);
-			return completionSource.Task;
-		}
-		
-		#if UNITY_EDITOR || DEVELOPMENT_BUILD
-		Advertisement.Initialize(GameID, true, this);
-		#else
-		Advertisement.Initialize(GameID, false, this);
-		#endif
-		
-		return completionSource.Task;
-	}
-
-	Task<bool> IAdsProvider.Interstitial()
-	{
-		return Show(InterstitialID);
-	}
-
-	Task<bool> IAdsProvider.Rewarded()
-	{
-		return Show(RewardedID);
-	}
-
-	async Task<bool> Show(string _PlacementID)
-	{
-		if (!Advertisement.isInitialized)
-			await Initialize();
-		
-		bool loaded = await Load(_PlacementID);
-		
-		if (!loaded)
-		{
-			Debug.LogFormat("[AdsProcessor] Second attempt to load placement. Placement: {0}.", _PlacementID);
-			
-			await Task.Delay(250);
-			
-			loaded = await Load(_PlacementID);
-		}
-		
-		if (!loaded)
-			return false;
-		
-		TaskCompletionSource<bool> completionSource = new TaskCompletionSource<bool>();
-		
-		m_ShowFinished[_PlacementID] = _Success => completionSource.TrySetResult(_Success);
-		
-		Advertisement.Show(_PlacementID, this);
-		
-		await completionSource.Task;
-		
-		await Task.Delay(250);
-		
-		return completionSource.Task.Result;
-	}
-
-	Task<bool> Load(string _PlacementID)
-	{
-		TaskCompletionSource<bool> completionSource = new TaskCompletionSource<bool>();
-		
-		m_LoadFinished[_PlacementID] = _Success => completionSource.TrySetResult(_Success);
-		
-		Advertisement.Load(_PlacementID, this);
-		
-		return completionSource.Task;
-	}
-
-	void InvokeLoadAdsFinished(bool _Success)
-	{
-		Action<bool> action = m_LoadAdsFinished;
-		m_LoadAdsFinished = null;
-		action?.Invoke(_Success);
-	}
-
-	void InvokeLoadFinished(string _PlacementID, bool _Success)
-	{
-		if (!m_LoadFinished.TryGetValue(_PlacementID, out Action<bool> action))
-			return;
-		
-		m_LoadFinished.Remove(_PlacementID);
-		
-		action?.Invoke(_Success);
-	}
-
-	void InvokeShowFinished(string _PlacementID, bool _Success)
-	{
-		if (!m_ShowFinished.TryGetValue(_PlacementID, out Action<bool> action))
-			return;
-		
-		m_ShowFinished.Remove(_PlacementID);
-		
-		action?.Invoke(_Success);
-	}
-
-	#region Unity Ads Implementation
-
-	void IUnityAdsInitializationListener.OnInitializationComplete()
-	{
-		Debug.Log("[AdsProcessor] Load ads success.");
-		
-		InvokeLoadAdsFinished(true);
-	}
-
-	void IUnityAdsInitializationListener.OnInitializationFailed(UnityAdsInitializationError _Error, string _Message)
-	{
-		Debug.LogErrorFormat("[AdsProcessor] Load ads failed. Error: {0} Message: {1}.", _Error, _Message);
-		
-		InvokeLoadAdsFinished(false);
-	}
-
-	void IUnityAdsLoadListener.OnUnityAdsAdLoaded(string _PlacementID)
-	{
-		Debug.LogFormat("[AdsProcessor] Load placement success. Placement: {0}.", _PlacementID);
-		
-		InvokeLoadFinished(_PlacementID, true);
-	}
-
-	void IUnityAdsLoadListener.OnUnityAdsFailedToLoad(string _PlacementID, UnityAdsLoadError _Error, string _Message)
-	{
-		Debug.LogErrorFormat("[AdsProcessor] Load placement failed. Placement: {0} Error: {1} Message: {2}.", _PlacementID, _Error, _Message);
-		
-		InvokeLoadFinished(_PlacementID, false);
-	}
-
-	void IUnityAdsShowListener.OnUnityAdsShowStart(string _PlacementID)
-	{
-		Debug.LogFormat("[AdsProcessor] Show placement. Placement: {0}.", _PlacementID);
-	}
-
-	void IUnityAdsShowListener.OnUnityAdsShowClick(string _PlacementID)
-	{
-		Debug.LogFormat("[AdsProcessor] Click placement. Placement: {0}.", _PlacementID);
-	}
-
-	void IUnityAdsShowListener.OnUnityAdsShowComplete(string _PlacementID, UnityAdsShowCompletionState _State)
-	{
-		Debug.LogFormat("[AdsProcessor] Show placement success. Placement: {0} State: {1}.", _PlacementID, _State);
-		
-		InvokeShowFinished(_PlacementID, _State == UnityAdsShowCompletionState.COMPLETED);
-	}
-
-	void IUnityAdsShowListener.OnUnityAdsShowFailure(string _PlacementID, UnityAdsShowError _Error, string _Message)
-	{
-		Debug.LogErrorFormat("[AdsProcessor] Show placement failed. Placement: {0} Error: {1} Message: {2}.", _PlacementID, _Error, _Message);
-		
-		InvokeShowFinished(_PlacementID, false);
-	}
-
-	#endregion
-}
+// [Preserve]
+// public class AdsProviderUnity : IAdsProvider, IUnityAdsInitializationListener, IUnityAdsLoadListener, IUnityAdsShowListener
+// {
+// 	#if UNITY_IOS
+// 	static string GameID         => "4234912";
+// 	static string InterstitialID => "Interstitial_iOS";
+// 	static string RewardedID     => "Rewarded_iOS";
+// 	#elif UNITY_ANDROID
+// 	static string GameID         => "4234913";
+// 	static string InterstitialID => "Interstitial_Android";
+// 	static string RewardedID     => "Rewarded_Android";
+// 	#endif
+//
+// 	string IAdsProvider.ID => "unity_ads";
+//
+// 	readonly Dictionary<string, Action<bool>> m_LoadFinished = new Dictionary<string, Action<bool>>();
+// 	readonly Dictionary<string, Action<bool>> m_ShowFinished = new Dictionary<string, Action<bool>>();
+//
+// 	Action<bool> m_LoadAdsFinished;
+// 	Action<bool> m_InterstitialFinished;
+// 	Action<bool> m_RewardedFinished;
+//
+// 	public Task<bool> Initialize()
+// 	{
+// 		TaskCompletionSource<bool> completionSource = new TaskCompletionSource<bool>();
+// 		
+// 		m_LoadAdsFinished = _Success => completionSource.TrySetResult(_Success);
+// 		
+// 		if (Advertisement.isInitialized)
+// 		{
+// 			InvokeLoadAdsFinished(true);
+// 			return completionSource.Task;
+// 		}
+// 		
+// 		if (!Advertisement.isSupported)
+// 		{
+// 			Debug.LogError("[AdsUnity] Initialize failed. Ads not supported.");
+// 			InvokeLoadAdsFinished(false);
+// 			return completionSource.Task;
+// 		}
+// 		
+// 		#if UNITY_EDITOR || DEVELOPMENT_BUILD
+// 		Advertisement.Initialize(GameID, true, this);
+// 		#else
+// 		Advertisement.Initialize(GameID, false, this);
+// 		#endif
+// 		
+// 		return completionSource.Task;
+// 	}
+//
+// 	Task<bool> IAdsProvider.Interstitial()
+// 	{
+// 		return Show(InterstitialID);
+// 	}
+//
+// 	Task<bool> IAdsProvider.Rewarded()
+// 	{
+// 		return Show(RewardedID);
+// 	}
+//
+// 	async Task<bool> Show(string _PlacementID)
+// 	{
+// 		if (!Advertisement.isInitialized)
+// 			await Initialize();
+// 		
+// 		bool loaded = await Load(_PlacementID);
+// 		
+// 		if (!loaded)
+// 		{
+// 			Debug.LogFormat("[AdsProcessor] Second attempt to load placement. Placement: {0}.", _PlacementID);
+// 			
+// 			await Task.Delay(250);
+// 			
+// 			loaded = await Load(_PlacementID);
+// 		}
+// 		
+// 		if (!loaded)
+// 			return false;
+// 		
+// 		TaskCompletionSource<bool> completionSource = new TaskCompletionSource<bool>();
+// 		
+// 		m_ShowFinished[_PlacementID] = _Success => completionSource.TrySetResult(_Success);
+// 		
+// 		Advertisement.Show(_PlacementID, this);
+// 		
+// 		await completionSource.Task;
+// 		
+// 		await Task.Delay(250);
+// 		
+// 		return completionSource.Task.Result;
+// 	}
+//
+// 	Task<bool> Load(string _PlacementID)
+// 	{
+// 		TaskCompletionSource<bool> completionSource = new TaskCompletionSource<bool>();
+// 		
+// 		m_LoadFinished[_PlacementID] = _Success => completionSource.TrySetResult(_Success);
+// 		
+// 		Advertisement.Load(_PlacementID, this);
+// 		
+// 		return completionSource.Task;
+// 	}
+//
+// 	void InvokeLoadAdsFinished(bool _Success)
+// 	{
+// 		Action<bool> action = m_LoadAdsFinished;
+// 		m_LoadAdsFinished = null;
+// 		action?.Invoke(_Success);
+// 	}
+//
+// 	void InvokeLoadFinished(string _PlacementID, bool _Success)
+// 	{
+// 		if (!m_LoadFinished.TryGetValue(_PlacementID, out Action<bool> action))
+// 			return;
+// 		
+// 		m_LoadFinished.Remove(_PlacementID);
+// 		
+// 		action?.Invoke(_Success);
+// 	}
+//
+// 	void InvokeShowFinished(string _PlacementID, bool _Success)
+// 	{
+// 		if (!m_ShowFinished.TryGetValue(_PlacementID, out Action<bool> action))
+// 			return;
+// 		
+// 		m_ShowFinished.Remove(_PlacementID);
+// 		
+// 		action?.Invoke(_Success);
+// 	}
+//
+// 	#region Unity Ads Implementation
+//
+// 	void IUnityAdsInitializationListener.OnInitializationComplete()
+// 	{
+// 		Debug.Log("[AdsProcessor] Load ads success.");
+// 		
+// 		InvokeLoadAdsFinished(true);
+// 	}
+//
+// 	void IUnityAdsInitializationListener.OnInitializationFailed(UnityAdsInitializationError _Error, string _Message)
+// 	{
+// 		Debug.LogErrorFormat("[AdsProcessor] Load ads failed. Error: {0} Message: {1}.", _Error, _Message);
+// 		
+// 		InvokeLoadAdsFinished(false);
+// 	}
+//
+// 	void IUnityAdsLoadListener.OnUnityAdsAdLoaded(string _PlacementID)
+// 	{
+// 		Debug.LogFormat("[AdsProcessor] Load placement success. Placement: {0}.", _PlacementID);
+// 		
+// 		InvokeLoadFinished(_PlacementID, true);
+// 	}
+//
+// 	void IUnityAdsLoadListener.OnUnityAdsFailedToLoad(string _PlacementID, UnityAdsLoadError _Error, string _Message)
+// 	{
+// 		Debug.LogErrorFormat("[AdsProcessor] Load placement failed. Placement: {0} Error: {1} Message: {2}.", _PlacementID, _Error, _Message);
+// 		
+// 		InvokeLoadFinished(_PlacementID, false);
+// 	}
+//
+// 	void IUnityAdsShowListener.OnUnityAdsShowStart(string _PlacementID)
+// 	{
+// 		Debug.LogFormat("[AdsProcessor] Show placement. Placement: {0}.", _PlacementID);
+// 	}
+//
+// 	void IUnityAdsShowListener.OnUnityAdsShowClick(string _PlacementID)
+// 	{
+// 		Debug.LogFormat("[AdsProcessor] Click placement. Placement: {0}.", _PlacementID);
+// 	}
+//
+// 	void IUnityAdsShowListener.OnUnityAdsShowComplete(string _PlacementID, UnityAdsShowCompletionState _State)
+// 	{
+// 		Debug.LogFormat("[AdsProcessor] Show placement success. Placement: {0} State: {1}.", _PlacementID, _State);
+// 		
+// 		InvokeShowFinished(_PlacementID, _State == UnityAdsShowCompletionState.COMPLETED);
+// 	}
+//
+// 	void IUnityAdsShowListener.OnUnityAdsShowFailure(string _PlacementID, UnityAdsShowError _Error, string _Message)
+// 	{
+// 		Debug.LogErrorFormat("[AdsProcessor] Show placement failed. Placement: {0} Error: {1} Message: {2}.", _PlacementID, _Error, _Message);
+// 		
+// 		InvokeShowFinished(_PlacementID, false);
+// 	}
+//
+// 	#endregion
+// }
 
 public class AdsProviderSnapshot
 {
