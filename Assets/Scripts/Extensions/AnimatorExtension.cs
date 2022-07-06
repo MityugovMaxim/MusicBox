dc47ad65cd0c21b91b3dec76c132cb32098beb34
MityugovMaxim <mityugovmaxim@gmail.com>
2022-07-06 11:58:29 +0300

    New extension function for Animator

--- Assets/Scripts/Extensions/AnimatorExtension.cs
+++ Assets/Scripts/Extensions/AnimatorExtension.cs
@@ -3,6 +3,16 @@ using UnityEngine;
 
 public static class AnimatorExtension
 {
+	public static void RegisterExit(this Animator _Animator, string _ID, Action _Action)
+	{
+		StateBehaviour.RegisterExit(_Animator, _ID, _Action);
+	}
+
+	public static void UnregisterExit(this Animator _Animator, string _ID, Action _Action)
+	{
+		StateBehaviour.UnregisterExit(_Animator, _ID, _Action);
+	}
+
 	public static void RegisterComplete(this Animator _Animator, string _ID, Action _Action)
 	{
 		StateBehaviour.RegisterComplete(_Animator, _ID, _Action);
